5b4f78a78a00ac0b66bea24c2c469a70f22792c7
yakreb <berkayturna@gmail.com>
2025-01-29 14:43:41 +0100

    add task management functionality and styling to ToDoList component

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 
 function ToDoList() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(["Walk the dog", "Go to the gym"]);
   const [newTask, setNewTask] = useState("");
 
-  function handleInputChange(event) {}
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
 
   function addTask() {}
 
@@ -14,9 +16,44 @@ function ToDoList() {
 
   function moveTaskDown(index) {}
   return (
-    <>
+    <div className="to-do-list">
       <h1>To-Do List</h1>
-    </>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newTask}
+          onChange={handleInputChange}
+        />
+        <button className="add-button" onClick={addTask}>
+          Add
+        </button>
+      </div>
+      <div>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <span className="text">{task}</span>
+              <button
+                className="remove-button"
+                onClick={() => removeTask(index)}
+              >
+                Remove
+              </button>
+              <button className="move-button" onClick={() => moveTaskUp(index)}>
+                Up
+              </button>
+              <button
+                className="move-button"
+                onClick={() => moveTaskDown(index)}
+              >
+                Down
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 export default ToDoList;
