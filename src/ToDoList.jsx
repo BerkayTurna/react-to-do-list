b5040a4b4fde92042fa082081e32ec4c1aa0ada3
yakreb <berkayturna@gmail.com>
2025-01-29 12:04:16 +0100

    implement initial state and task management functions in ToDoList component

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 
 function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {}
+
+  function addTask() {}
+
+  function removeTask(index) {}
+
+  function moveTaskUp(index) {}
+
+  function moveTaskDown(index) {}
   return (
     <>
       <h1>To-Do List</h1>
